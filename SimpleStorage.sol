1edb049ac86d978e9993b32787e7b59d23205205
pocotoshiba <Benjava93@outlook.com>
2026-03-26 22:17:59 +0000

    Add files via upload

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,42 +1,34 @@
-# SimpleStorage Solidity Contract
-
-This is a very simple Solidity smart contract that:
-
-- Stores a single favorite number
-- Stores a list of people (name + favorite number)
-- Lets you look up a person's favorite number by their name
-
-It is written for Solidity version `^0.8.18`.
-
----
-
-## Contract Overview
-
-**File:** `SimpleStorage.sol`
-
-```solidity
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// This contract stores one number and a list of people
 contract SimpleStorage {
+    // Main number we store on the blockchain
     uint256 myFavoriteNumber;
 
+    // A person has a favorite number and a name
     struct Person {
         uint256 favoriteNumber;
         string name;
     }
 
+    // A list of all people added
     Person[] public listOfPeople;
+
+    // Look up a person's favorite number by their name
     mapping(string => uint256) public nameToFavoriteNumber;
 
+    // Save a number as the main favorite number
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
+    // Read the main favorite number
     function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
     }
 
+    // Add a new person and remember their favorite number
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
